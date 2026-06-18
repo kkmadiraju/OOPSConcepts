faad793058c8d3a396acf553448aa237299449b0
Kiran Kumar Madiraju <ssamadiraju@gmail.com>
2026-06-18 16:54:42 +0530

    Add project files.

---
+++ OOPSConcepts/ClassA.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace OOPSConcepts
+{
+    internal class ClassA
+    {
+        static int a = 10;
+        public ClassA()
+        {
+            Console.WriteLine("Constructor of ClassA",a);
+        }
+        //You cannot have a static constructor with parameters.
+        //Static constructors are parameterless and are used to initialize static members of the class.
+        //They are called automatically before any instance of the class is created or any static members are accessed.
+        //private ClassA()
+        //{
+        //    Console.WriteLine("Private Constructor of ClassA");
+        //}
+
+        static ClassA()
+        {
+            int a = 1000;
+            Console.WriteLine("Static Constructor of ClassA",a);
+        }
+        public ClassA(int a, int b)
+        {
+            Console.WriteLine(a+b);
+        }
+    }
+    class ClassB: ClassA
+    {
+        public ClassB()
+        {
+            
+        }
+    }
+    class ClassC : ClassB
+    {
+        public ClassC()
+        {
+            
+        }
+    }
+}
---
+++ OOPSConcepts/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace OOPSConcepts
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Create instances to demonstrate constructors
+            ClassA a1 = new ClassA();
+            ClassA a2 = new ClassA(3, 4);
+            ClassB b = new ClassB();
+            ClassC c = new ClassC();
+
+            Console.WriteLine("Program finished.");
+        }
+    }
+}
